a517b3fcaf3e889f7e5978d837b7391b3e6e9149
tauri_s <tauristclaire@gmail.com>
2023-12-06 16:33:10 -0800

    Declare variable to keep track of state of the food item typed into the input and set initial value of useState to empty string

--- src/NewFoodInput.js
+++ src/NewFoodInput.js
@@ -2,6 +2,10 @@ import React from "react";
 import React, { useState } from "react";
 
 const NewFoodInput = () => {
+  /*create a variable to keep track of the state of the food 
+  item the user types in the input. Set the initial value of 
+  the useState() to an empty string: ("").*/
+  const [ food, setFood ] = useState("");
   return (
     <form className="food-form">
       <input type="text" />

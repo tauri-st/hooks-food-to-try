82aacf40bda75af4ca23bb62893c8bb6c9b761cd
tauri_s <tauristclaire@gmail.com>
2023-12-06 17:35:15 -0800

    Add parentheses around empty array state in App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import "./styles.css";
 
 export default function App() {
   //5.Create state with an initial value of an empty array
-  const [ foodsToTry, setFoodsToTry ] = useState[];
+  const [ foodsToTry, setFoodsToTry ] = useState([]);
   //6. Add a function to accept a food and add it to the foodsToTry array"
   function addFoodItem (food) {
     foodsToTry.concat(food);

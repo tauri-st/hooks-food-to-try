f9cfb1c0fc4441494fa2f4d60f1379c14781561c
tauri_s <tauristclaire@gmail.com>
2023-12-11 16:34:42 -0800

    Add onClick event handler that fires the deleteFood function

--- src/Food.js
+++ src/Food.js
@@ -3,7 +3,7 @@ import React from "react";
 //C. Accept food as a prop from App.js and use it in the JSX
 const Food = (props) => {
     return <li>{props.food}
-    <span class="delete">X</span></li>
+    <span class="delete" onClick={deleteFood}>X</span></li>
     
 };
 

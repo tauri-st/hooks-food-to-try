e7db42cc742b4246a57c29ee82b413764bdbdb36
tauri_s <tauristclaire@gmail.com>
2023-12-11 16:54:52 -0800

    Add method to Food component to delete foods and call this in onClick method assigned to the X of each food item

--- src/Food.js
+++ src/Food.js
@@ -2,8 +2,13 @@ import React from "react";
 
 //C. Accept food as a prop from App.js and use it in the JSX
 const Food = (props) => {
+
+    const onDelete = () => {
+        props.deleteFood(props.food)
+    };
+
     return <li>{props.food}
-    <span className="delete" onClick={deleteFood}>X</span></li>
+    <span className="delete" onClick={onDelete}>X</span></li>
     
 };
 

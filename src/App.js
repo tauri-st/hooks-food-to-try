c9aaa288394c2cdb5c725ba948628271986cd8f3
tauri_s <tauristclaire@gmail.com>
2023-12-11 16:36:29 -0800

    Move exclamation mark for includes method within the filter method of updatedFoods

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ export default function App() {
 
   const deleteFood = (foodToDelete) => {
     const updatedFoods = foodsToTry.filter(function (item) {
-      return item.!includes(foodToDelete);
+      return !item.includes(foodToDelete);
     })
     setFoodsToTry(updatedFoods);
 

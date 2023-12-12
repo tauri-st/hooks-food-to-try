01a0dc7d79e0021c04d325dca470b8efaf1a1272
tauri_s <tauristclaire@gmail.com>
2023-12-11 16:28:45 -0800

    Add a return of X to delete food to Food component

--- src/Food.js
+++ src/Food.js
@@ -2,7 +2,9 @@ import React from "react";
 
 //C. Accept food as a prop from App.js and use it in the JSX
 const Food = (props) => {
-    return <li>{props.food}</li>
+    return <li>{props.food}
+    <span class="delete">X</span></li>
+    
 };
 
 export default Food;

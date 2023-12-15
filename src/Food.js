9a25bb43a29d7b39efb1f7caadff33e18d3e2fee
tauri_s <tauristclaire@gmail.com>
2023-12-15 14:09:36 -0800

    Remove dele functionality from Food.js and destructure prop name into the Food component

--- src/Food.js
+++ src/Food.js
@@ -1,14 +1,9 @@
 import React from "react";
 
 //C. Accept food as a prop from App.js and use it in the JSX
-const Food = (props) => {
+const Food = ({name}) => {
 
-    const onDelete = () => {
-        props.deleteFood(props.food)
-    };
-
-    return <li>{props.food}
-    <span className="delete" onClick={onDelete}>X</span></li>
+    return <li>{name}</li>
     
 };
 

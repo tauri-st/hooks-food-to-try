847162ddb7523bdb865f589d9c960929ba7a8a3d
tauri_s <tauristclaire@gmail.com>
2023-12-15 14:23:55 -0800

    Add import of useState and create state for tried food

--- src/Food.js
+++ src/Food.js
@@ -1,7 +1,9 @@
-import React from "react";
+import React, {useState} from "react";
 
 //C. Accept food as a prop from App.js and use it in the JSX
 const Food = ({name}) => {
+
+    const [tried, setTried] = useState(false);
 
     return <li>{name}</li>
     

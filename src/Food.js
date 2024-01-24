2c4d90d43faee3e49b7a7f9bc25980e020656eb2
tauri_s <tauristclaire@gmail.com>
2024-01-24 15:44:41 -0800

    Add notes to Food where deleteFood function will be called

--- src/Food.js
+++ src/Food.js
@@ -8,6 +8,16 @@ const Food = (props) => {
     };
 
     return <li>{props.food}
+    {/*H. Add a span element with a class of delete.*/}
+    {/*I. Add an onClick event handler to the span which will 
+    fire (AKA call) the deleteFood function when passed as a 
+    prop to the Food component from App.js. Remember that the 
+    deleteFood function needs a food item which you also have 
+    access to via props. You can either do this by creating a 
+    separate handleClick function that will call the deleteFood 
+    function and have your onClick event fire that separate 
+    function. Or, you can pass a callback function to the 
+    onClick event directly that will call the deleteFood function. */}
     <span className="delete" onClick={onDelete}>X</span></li>
     
 };
